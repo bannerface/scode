972e7ac15057b460be696ff629e04f3b969066da
bannerface <bannerface@gmail.com>
2012-02-02 18:33:58 +0800

    class2

---
+++ scode/Class2.h
@@ -0,0 +1,13 @@
+//
+//  Class2.h
+//  scode
+//
+//  Created by Jian Gao on 12-2-2.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+
+
+@interface Class2 : UIViewController
+
+@end
